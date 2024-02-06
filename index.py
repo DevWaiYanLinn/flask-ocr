7b64f1f0b859655c614de18ca1de7f5ce4e8e750
WaiYanLinn <waiyanlinn.lion@outlook.com>
2024-02-06 14:16:59 +0630

    update

--- index.py
+++ index.py
@@ -2,35 +2,35 @@ import flask
 
 print(flask.__version__)
 
-# from flask import Flask, jsonify
-# import os
-# from flask import Flask, flash, request, redirect, url_for
-# from werkzeug.utils import secure_filename
+from flask import Flask, jsonify
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
-# ALLOWED_EXTENSIONS = {"txt", "pdf", "png", "jpg", "jpeg", "gif"}
-# from flask import render_template
+ALLOWED_EXTENSIONS = {"txt", "pdf", "png", "jpg", "jpeg", "gif"}
+from flask import render_template
 
-# app = Flask(__name__)
-# app.secret_key = '1234'
-
-
-# @app.route("/")
-# def index():
-#     return render_template("index.html")
+app = Flask(__name__)
+app.secret_key = '1234'
 
 
-# def allowed_file(filename):
-#     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 
-# @app.route("/api/photo-upload", methods=["POST"])
-# def upload_file():
-#     if "file" not in request.files:
-#         flash("No file part")
-#         return redirect(request.url)
-#     file = request.files["file"]
-#     if file.filename == "":
-#         flash("No selected file")
-#         return redirect(request.url)
-#     if file and allowed_file(file.filename):
-#         return 'hello world'
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/api/photo-upload", methods=["POST"])
+def upload_file():
+    if "file" not in request.files:
+        flash("No file part")
+        return redirect(request.url)
+    file = request.files["file"]
+    if file.filename == "":
+        flash("No selected file")
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        return 'hello world'

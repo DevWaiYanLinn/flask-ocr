f0bf64414e05a3087144fd7ff620836fb5e901a2
WaiYanLinn <waiyanlinn.lion@outlook.com>
2024-02-06 14:09:59 +0630

    initial

---
+++ index.py
@@ -0,0 +1,36 @@
+import flask
+
+print(flask.__version__)
+
+# from flask import Flask, jsonify
+# import os
+# from flask import Flask, flash, request, redirect, url_for
+# from werkzeug.utils import secure_filename
+
+# ALLOWED_EXTENSIONS = {"txt", "pdf", "png", "jpg", "jpeg", "gif"}
+# from flask import render_template
+
+# app = Flask(__name__)
+# app.secret_key = '1234'
+
+
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
+
+
+# def allowed_file(filename):
+#     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+# @app.route("/api/photo-upload", methods=["POST"])
+# def upload_file():
+#     if "file" not in request.files:
+#         flash("No file part")
+#         return redirect(request.url)
+#     file = request.files["file"]
+#     if file.filename == "":
+#         flash("No selected file")
+#         return redirect(request.url)
+#     if file and allowed_file(file.filename):
+#         return 'hello world'
